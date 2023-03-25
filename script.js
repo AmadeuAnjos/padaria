13ebd5b6bc5dd446ae8ed3a3fe677e7f6c23e2f4
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-03-24 21:16:53 -0300

    :iphone: alterando links e redimensionamnento

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function clickMenu() {
         menu.style.display = 'block'
     }
 }
+
+function clickChat() {  
+
+    console.log('teste')
+    /* if(menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+     */
+}
